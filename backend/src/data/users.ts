24846fa96797957e6fc3af5c312913800e93fe5d
Ripon-Mardy <mardyripon10@gmail.com>
2026-03-17 17:41:09 +0600

    add json data

--- backend/src/data/users.ts
+++ backend/src/data/users.ts
@@ -1,17 +1,23 @@
-
 export const users: any[] = [
   {
     id: 1,
-    email: "admin@test.com",
-    password: "123456",
-    role: "admin",
-    permissions: ["dashboard.view", "users.view", "reports.view"],
+    email: 'admin@test.com',
+    password: '123456',
+    role: 'admin',
+    permissions: ['dashboard.view', 'users.view', 'reports.view'],
   },
   {
     id: 2,
-    email: "agent@test.com",
-    password: "123456",
-    role: "agent",
-    permissions: ["dashboard.view"],
+    email: 'agent@test.com',
+    password: '123456',
+    role: 'agent',
+    permissions: ['dashboard.view'],
+  },
+  {
+    id: 3,
+    email: 'agent@test1.com',
+    password: '123456',
+    role: 'agent',
+    permissions: ['dashboard.view'],
   },
 ];
