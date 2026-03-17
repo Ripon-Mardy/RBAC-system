6a1f1aba009fedcc11cb2dae9a6926fd8ad87588
Ripon-Mardy <mardyripon10@gmail.com>
2026-03-17 17:49:34 +0600

    fix cors problem

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
   app.enableCors({
-    origin: '*',
+    origin: true,
+    Credential: true,
   });
 }
 bootstrap();
